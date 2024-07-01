13e4cca4a00448107f0de019886487b8fb8e718d
Andrei Ana-Maria <ana.maria.andrei@ibm.com>
2024-07-01 13:47:03 +0300

    added ticket form

---
+++ app/tickets/new/page.tsx
@@ -0,0 +1,8 @@
+import TicketForm from "@/components/TicketForm";
+import React from "react";
+
+const NewTicket = () => {
+  return <TicketForm></TicketForm>;
+};
+
+export default NewTicket;
---
+++ components/TicketForm.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { ticketSchema } from "@/ValidationSchemas/ticket";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+type TicketFormData = z.infer<typeof ticketSchema>;
+const TicketForm = () => {
+  const form = useForm<TicketFormData>({
+    resolver: zodResolver(ticketSchema),
+  });
+
+  async function onSubmit(values: z.infer<typeof ticketSchema>) {
+    console.log(values);
+  }
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ticket Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Ticket title" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Controller
+            name="description"
+            control={form.control}
+            render={({ field }) => (
+              <SimpleMDE placeholder="Description..." {...field} />
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default TicketForm;

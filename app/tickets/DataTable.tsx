9dafcfb2b80c6adfdde338b7ee2109b2d41ae104
Andrei Ana-Maria <ana.maria.andrei@ibm.com>
2024-07-01 12:49:25 +0300

    added ticket listing

---
+++ app/tickets/DataTable.tsx
@@ -0,0 +1,73 @@
+import { Ticket } from "@prisma/client";
+import React from "react";
+import TicketStatusBadge from "@/components/TicketStatusBadge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import TicketPriority from "@/components/TicketPriority";
+
+interface Props {
+  tickets: Ticket[];
+}
+
+const DataTable = ({ tickets }: Props) => {
+  return (
+    <div className="w-full mt-5">
+      <div className="rounded-md sm:border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>
+                <div className="flex justify-center">Status</div>
+              </TableHead>
+              <TableHead>
+                <div className="flex justify-center">Priority</div>
+              </TableHead>
+              <TableHead>Created At</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tickets
+              ? tickets.map((ticket) => (
+                  <TableRow key={ticket.id} data-href="/">
+                    <TableCell>{ticket.title}</TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketStatusBadge
+                          status={ticket.status}
+                        ></TicketStatusBadge>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketPriority
+                          priority={ticket.priority}
+                        ></TicketPriority>
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      {ticket.createdAt.toLocaleDateString("en-US", {
+                        year: "2-digit",
+                        month: "2-digit",
+                        day: "2-digit",
+                        hour: "numeric",
+                        minute: "2-digit",
+                      })}
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default DataTable;
